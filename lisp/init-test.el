143d4edbf7b8b8834ed691369bf0b694239d3df1
flygerzy <zhangyangchn@outlook.com>
2022-06-06 16:46:49 +0800

    add package marginalia

--- lisp/init-test.el
+++ lisp/init-test.el
@@ -260,7 +260,22 @@
   ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
   ;;;; 4. locate-dominating-file
   ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
-)
+  )
+
+;; --------------------------------------------------------------------------------
+;; Enable richer annotations using the Marginalia package
+(use-package marginalia
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
 
 ;; --------------------------------------------------------------------------------
 (provide 'init-test)

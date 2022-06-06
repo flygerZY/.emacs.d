a2a5f65fc85ec966af50f48d2c0b6e457c229134
flygerzy <zhangyangchn@outlook.com>
2022-06-06 14:31:16 +0800

    add consult vertico orderless

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -35,38 +35,38 @@
 
 
 
-;; ivy counsel swiper
-(use-package ivy
-  :ensure t
-  :diminish (ivy-mode)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "%d/%d ")
-  (setq ivy-display-style 'fancy))
-
-(use-package counsel	     
-:ensure t		     
-  :bind			     
-  (("M-y" . counsel-yank-pop)
-   :map ivy-minibuffer-map   
-   ("M-y" . ivy-next-line))) 
-
-(use-package swiper
-  :ensure t
-  :bind (("C-s" . swiper-isearch)
-	 ("C-r" . swiper-isearch)
-	 ("C-c C-r" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq ivy-display-style 'fancy)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
+;;;;; ivy counsel swiper
+;;;(use-package ivy
+;;;  :ensure t
+;;;  :diminish (ivy-mode)
+;;;  :bind (("C-x b" . ivy-switch-buffer))
+;;;  :config
+;;;  (ivy-mode 1)
+;;;  (setq ivy-use-virtual-buffers t)
+;;;  (setq ivy-count-format "%d/%d ")
+;;;  (setq ivy-display-style 'fancy))
+;;;
+;;;(use-package counsel	     
+;;;:ensure t		     
+;;;  :bind			     
+;;;  (("M-y" . counsel-yank-pop)
+;;;   :map ivy-minibuffer-map   
+;;;   ("M-y" . ivy-next-line))) 
+;;;
+;;;(use-package swiper
+;;;  :ensure t
+;;;  :bind (("C-s" . swiper-isearch)
+;;;	 ("C-r" . swiper-isearch)
+;;;	 ("C-c C-r" . ivy-resume)
+;;;	 ("M-x" . counsel-M-x)
+;;;	 ("C-x C-f" . counsel-find-file))
+;;;  :config
+;;;  (progn
+;;;    (ivy-mode 1)
+;;;    (setq ivy-use-virtual-buffers t)
+;;;    (setq ivy-display-style 'fancy)
+;;;    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+;;;    ))
 
 (use-package markdown-mode
   :ensure t

13fd89e6edbfc7637149e0ddf41ea7149cacafba
flygerzy <zhangyangchn@outlook.com>
2022-06-20 08:52:28 +0800

    version 0.1 create

--- lisp/custom.el
+++ lisp/custom.el
@@ -30,9 +30,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+   '("566c61d0e40a728d5b631ac276017ace6866574ee58a4b6fbc770e436e481d57" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "3319c893ff355a88b86ef630a74fad7f1211f006d54ce451aab91d35d018158f" "67f0f440afa2e68d9d00219b5a56308761af45832fb60769d2b2fd36e3fead45" "aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(package-selected-packages
-   '(counsel-projectile projectile keycast embark-consult embark marginalia consult orderless vertico awesome-tab youdao-dictionary which-key use-package undo-tree try treemacs-icons-dired restart-emacs rainbow-delimiters org-bullets neotree mwim move-dup markdown-mode magit info-colors highlight-symbol git-gutter expand-region doom-modeline diredfl diminish delight dashboard counsel company benchmark-init auto-package-update all-the-icons-ivy all-the-icons-dired)))
+   '(symbol-overlay counsel-projectile projectile keycast embark-consult embark marginalia consult orderless vertico awesome-tab youdao-dictionary which-key use-package undo-tree try treemacs-icons-dired restart-emacs rainbow-delimiters org-bullets neotree mwim move-dup markdown-mode magit info-colors highlight-symbol git-gutter expand-region doom-modeline diredfl diminish delight dashboard counsel company benchmark-init auto-package-update all-the-icons-ivy all-the-icons-dired))
+ '(warning-suppress-log-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-better-default.el
+++ lisp/init-better-default.el
@@ -7,6 +7,11 @@
 ;;开启servermode
 (server-mode 1)
 
+;; keep a list of recently opened files
+(require 'recentf)
+(recentf-mode 1)
+
+
 ;; System default coding
 (set-language-environment 'utf-8)
 
@@ -40,11 +45,49 @@
 (setq-default tab-width 4)                  ;tab4个字符宽度
 (setq indent-line-function 'insert-tab)     ;缩进时插入tab
 
-    
-
+   
 ;; use short answers for YES/NO ect.
 (setq use-short-answers t)
 
+;; --------------------------------------------------------------------------------
+;; make ibuffer default and show better
+(defalias 'list-buffers 'ibuffer)
+(setq ibuffer-saved-filter-groups
+	(quote (("default"
+		 ("dired" (mode . dired-mode))
+		 ("org" (name . "^.*org$"))
+		 ("magit" (mode . magit-mode))
+		 ("web" (or (mode . web-mode) (mode . js2-mode)))
+		 ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
+		 ("programming" (or
+				 (mode . verilog-mode)
+                                 (mode . c-mode)
+				 (mode . ess-r-mode)
+				 (mode . python-mode)
+				 (mode . c++-mode)))
+		 ("emacs" (or
+			   (name . "^\\*scratch\\*$")
+			   (name . "^\\*Messages\\*$")))
+		 ))))
+  (add-hook 'ibuffer-mode-hook
+	    (lambda ()
+	      (ibuffer-auto-mode 1)
+	      (ibuffer-switch-to-saved-filter-groups "default")))
+
+;; --------------------------------------------------------------------------------
+;; 解决windows 下emacs client打开中文文件乱码
+(when (eq system-type 'windows-nt)
+  (setq locale-coding-system 'gb18030)  ;此句保证中文字体设置有效
+  (setq w32-unicode-filenames 'nil)       ; 确保file-name-coding-system变量的设置不会无效
+  (setq file-name-coding-system 'gb18030) ; 设置文件名的编码为gb18030
+  )
+
+;; --------------------------------------------------------------------------------
+;; --------------------------------------------------------------------------------
+;; Forces the messages to 0, and kills the *Messages* buffer - thus disabling it on startup.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+;; --------------------------------------------------------------------------------
 (provide 'init-better-default)
 ;;; init-better-default.el ends here
 ;; Local Variables:
